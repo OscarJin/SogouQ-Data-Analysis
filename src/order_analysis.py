3c9a7edc5e90911e80ccb8ec5a2ffd20ae29b2a6
Oscar Jin <35246973+OscarJin@users.noreply.github.com>
2022-05-21 23:56:16 +0800

    Update order_analysis.py
    Add TODO

--- src/order_analysis.py
+++ src/order_analysis.py
@@ -6,12 +6,18 @@ warnings.filterwarnings('ignore')
 
 sogou = pd.read_csv("data/SogouQ.csv")
 
+# TODO: 计算出日志数据的Order平均值
+
 order_mean = mean(sogou['用户点击的顺序号'])
 print(order_mean)
+
+# TODO: 分析Order的频数，将Order频数排名前十位的数据表写出
 
 order_count = sogou.value_counts(sogou['用户点击的顺序号']).head(n=10)
 print(order_count)
 order_count.to_csv("result/click_order_top10.txt", sep='\t', header=False)
+
+# TODO: 对排名前十位的Order和频数，绘制出数据分布饼状图
 
 plt.pie(order_count, labels=order_count.index, autopct='%0.1f%%', pctdistance=0.7)
 plt.title("Top 10 Click Order")
