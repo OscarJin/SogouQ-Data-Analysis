6bf6b8871a9928521e8780ffa6f883d83e9b2168
JamesGuo <77781201+guojyjames@users.noreply.github.com>
2022-05-22 09:55:09 +0800

    Add files via upload

---
+++ src/task3_3.py
@@ -0,0 +1,16 @@
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+sogou = pd.read_csv("SogouQ.csv", sep=',')
+# for i in range(len(sogou)):
+#     sogou.loc[i,'ID']=str(sogou.loc[i,'ID'])
+columns=['Id', 'Word','Merge']
+dates=[]
+data=pd.DataFrame(columns=columns,index=dates)
+data['Id']=sogou['ID']
+data['Word']=sogou['word']
+for i in range(len(data)):
+    data.loc[i,'Merge']=[data.loc[i,'Id'],data.loc[i,'Word']]
+data1=data['Merge']
+print(data1)
+data1.to_csv("processed_data.csv",sep=",",index=True, header = True)
