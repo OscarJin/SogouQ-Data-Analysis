d161f691c29333ce9278241d889dab0d197c9afd
Oscar Jin <35246973+OscarJin@users.noreply.github.com>
2022-05-22 10:07:56 +0800

    reformat task3.py

--- src/task3_3.py
+++ src/task3_3.py
@@ -1,16 +1,17 @@
 import warnings
-warnings.filterwarnings('ignore')
 import pandas as pd
+warnings.filterwarnings('ignore')
+
 sogou = pd.read_csv("SogouQ.csv", sep=',')
 # for i in range(len(sogou)):
 #     sogou.loc[i,'ID']=str(sogou.loc[i,'ID'])
-columns=['Id', 'Word','Merge']
-dates=[]
-data=pd.DataFrame(columns=columns,index=dates)
+columns = ['Id', 'Word', 'Merge']
+dates = []
+data = pd.DataFrame(columns=columns,index=dates)
 data['Id']=sogou['ID']
 data['Word']=sogou['word']
 for i in range(len(data)):
-    data.loc[i,'Merge']=[data.loc[i,'Id'],data.loc[i,'Word']]
-data1=data['Merge']
+    data.loc[i, 'Merge'] = [data.loc[i, 'Id'], data.loc[i, 'Word']]
+data1 = data['Merge']
 print(data1)
-data1.to_csv("processed_data.csv",sep=",",index=True, header = True)
+data1.to_csv("processed_data.csv", sep=",", index=True, header=True)
