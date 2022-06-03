9f5d7de854cb1388bc38eaf20ff7b25e9dd9a3b3
Oscar Jin <35246973+OscarJin@users.noreply.github.com>
2022-06-03 14:20:24 +0800

    Update Total_words.py
    reformat

--- src/Total_words.py
+++ src/Total_words.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
-data = pd.read_csv('keywords.txt', sep="\t",names=['word','Frequency'])
+
+data = pd.read_csv('keywords.txt', sep="\t", names=['word', 'Frequency'])
 print(data)
 print(data.describe())
-data['target']=0
+data['target'] = 0
 conditions = [
-    data['Frequency'] >=500,
-    (data['Frequency'] <500 )& (data['Frequency']>=200),
-    (data['Frequency'] <200) & (data['Frequency']>=50),
-    (data['Frequency'] <50) & (data['Frequency']>=10)]
-rating = [4,3,2,1]
+    data['Frequency'] >= 500,
+    (data['Frequency'] < 500) & (data['Frequency'] >= 200),
+    (data['Frequency'] < 200) & (data['Frequency'] >= 50),
+    (data['Frequency'] < 50) & (data['Frequency'] >= 10)]
+rating = [4, 3, 2, 1]
 data['rating'] = np.select(conditions, rating, default=0)
 print(data)
 print(data.rating.value_counts())
-data2=data[data['rating']<4]
-bx = sns.boxplot(x="rating", y='Frequency', data =data2)
-bx.set(xlabel='Word Label', ylabel='Word Frequency', title='Word Frequency With Different Labels'
+data2 = data[data['rating'] < 4]
+bx = sns.boxplot(x="rating", y='Frequency', data=data2)
+bx.set(xlabel='Word Label', ylabel='Word Frequency', title='Word Frequency With Different Labels')
