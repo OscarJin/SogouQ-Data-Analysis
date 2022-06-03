b051fdc224c6ad6883d2eb72c35133f9b259d991
Oscar Jin <35246973+OscarJin@users.noreply.github.com>
2022-06-03 14:52:44 +0800

    Update total_words.py

--- src/total_words.py
+++ src/total_words.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('data/keywords.txt', sep="\t", names=['word', 'Frequency'])
 print(data.head())
 print(data.describe())
-data['target'] = 0
+
+data_100 = data.head(n=100)
+plt.plot(data_100.index, data_100['Frequency'])
+plt.title("Keyword Frequency Distribution")
+plt.xlabel("Keyword Ranking")
+plt.ylabel("Frequency")
+plt.show()
+
 conditions = [
     data['Frequency'] >= 500,
     (data['Frequency'] < 500) & (data['Frequency'] >= 200),
@@ -18,3 +26,4 @@ print(data.rating.value_counts())
 data2 = data[data['rating'] < 4]
 bx = sns.boxplot(x="rating", y='Frequency', data=data2)
 bx.set(xlabel='Word Label', ylabel='Word Frequency', title='Word Frequency With Different Labels')
+plt.show()
